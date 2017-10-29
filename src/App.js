d70bc3fa5bebd33385e7e6b796759b332eee39e5
GRSGreck <GrechukRomanSergeevich@gmail.com>
2017-10-29 23:02:55 +0200

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const user = {
-    firstName: 'Roman',
-    lastName: 'Hrechuk',
-};
-
 class App extends Component {
-    formatName = user => `${ user.firstName } ${ user.lastName }`
 
     render() {
         return (
-            <h1>Hello, { this.formatName(user) }</h1>
+            <h1>Hello, World!!!</h1>
         );
     }
 }
